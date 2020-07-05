2ccf217784d6d7fb9d6794746af3659044438ad8
bennyboer <barny.eder@gmail.com>
2020-07-05 13:26:07 +0200

    [Export] Simple underline support

--- export/src/main/java/de/be/thaw/export/pdf/element/impl/TextElementExporter.java
+++ export/src/main/java/de/be/thaw/export/pdf/element/impl/TextElementExporter.java
@@ -3,10 +3,14 @@ package de.be.thaw.export.pdf.element.impl;
 import de.be.thaw.export.exception.ExportException;
 import de.be.thaw.export.pdf.ExportContext;
 import de.be.thaw.export.pdf.element.ElementExporter;
+import de.be.thaw.text.model.emphasis.TextEmphasis;
+import de.be.thaw.text.model.tree.NodeType;
+import de.be.thaw.text.model.tree.impl.FormattedNode;
 import de.be.thaw.typeset.page.Element;
 import de.be.thaw.typeset.page.ElementType;
 import de.be.thaw.typeset.page.impl.TextElement;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 
 import java.io.IOException;
 import java.util.Set;
@@ -32,16 +36,31 @@ public class TextElementExporter implements ElementExporter {
         try {
             out.beginText();
 
+            PDFont font = ctx.getFontForNode(te.getNode());
             double fontSize = ctx.getFontSizeForNode(te.getNode());
 
             double y = ctx.getCurrentPage().getMediaBox().getUpperRightY() - te.getPosition().getY();
 
-            out.setFont(ctx.getFontForNode(te.getNode()), (float) fontSize);
+            out.setFont(font, (float) fontSize);
             out.newLineAtOffset((float) te.getPosition().getX(), (float) y);
 
             out.showText(te.getText());
 
             out.endText();
+
+            if (te.getNode().getType() == NodeType.FORMATTED) {
+                FormattedNode fn = (FormattedNode) te.getNode();
+
+                if (fn.getEmphases().contains(TextEmphasis.UNDERLINED)) {
+                    double descent = font.getFontDescriptor().getDescent() / 1000 * fontSize;
+                    double lineY = y + descent;
+
+                    out.setLineWidth(font.getFontDescriptor().getFontWeight() / 1000);
+                    out.moveTo((float) te.getPosition().getX(), (float) lineY);
+                    out.lineTo((float) (te.getPosition().getX() + te.getSize().getWidth()), (float) lineY);
+                    out.stroke();
+                }
+            }
         } catch (IOException e) {
             throw new ExportException("Text element could not be exported to PDF due to another exception", e);
         }
